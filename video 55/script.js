2cbc661a9db7d5bd4b1840e7a6e23ea8f2770035
PranayAWani <pranaywani04@gmail.com>
2023-12-15 14:10:28 +0530

    java

---
+++ video 55/script.js
@@ -0,0 +1,9 @@
+console.log("Hey this is a tutorial 55 of sigma web development coures")
+var a=5;
+var b=6;
+var c="harry";
+
+console.log(a+b+8)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
