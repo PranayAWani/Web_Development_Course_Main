672bba1539119465cb078ad481898d8add4814ac
PranayAWani <pranaywani04@gmail.com>
2023-12-21 22:33:02 +0530

    javascript

---
+++ video 54/script.js
@@ -0,0 +1,14 @@
+console.log("Hey this is a tutorial 55 of sigma web development coures")
+var a=5;
+var b=6;
+var c="harry";
+var d=prompt("Enter a NUmber");
+console.log("Your number is " + a)
+
+var istrue =confirm("Are you sure u want to blast the cp");
+if(istrue){
+    console.log("Your comouter is blasting")
+}
+else{
+    console.log("Your computer is not blasting")
+}
