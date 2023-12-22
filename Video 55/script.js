e3b9c84342329b4a831468f8c2a0cb507e016b47
PranayAWani <pranaywani04@gmail.com>
2023-12-22 21:33:15 +0530

    java

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,1 +1,35 @@
 console.log("Hey this is tutorial ");
+var a=45;
+var b=54;
+var c="Pranay"
+console.log(a+b+8);
+console.log(typeof a, typeof b, typeof c);
+var d=54;
+{
+    let d=87;
+    console.log("INside the block");
+    console.log(d);
+}
+console.log("Outside the block");
+console.log(d);
+
+const a1=98;
+
+let e=98;
+let f="pranay";
+let g;
+let h=true;
+let i=123456789;
+
+console.log(e ,f,g,h,i)
+console.log( typeof e , typeof f, typeof g, typeof h, typeof i)
+
+let o={
+    name : "Pranay",
+    "job code": 1234
+}
+console.log(o);
+o.salary= "100cr"
+console.log(o);
+o.salary="500cr"
+console.log(o);
