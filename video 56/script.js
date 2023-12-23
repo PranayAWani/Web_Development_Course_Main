38f9317f6ae72043d019f3e052c0663264045255
PranayAWani <pranaywani04@gmail.com>
2023-12-23 10:55:46 +0530

    java

---
+++ video 56/script.js
@@ -0,0 +1,15 @@
+console.log("HEllo WElocme to the tutorial ");
+let age=15;
+let grace=2;
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age*grace)
+console.log(age**grace)
+if((age+grace)>18){
+        console.log("you can Drive");
+}
+else{
+    console.log("You cannot drive");
+}
