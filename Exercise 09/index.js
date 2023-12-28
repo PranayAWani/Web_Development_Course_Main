d15d0ce13856345e581b564b2deee32973556f89
PranayAWani <pranaywani04@gmail.com>
2023-12-28 18:42:58 +0530

    exercise 9

---
+++ Exercise 09/index.js
@@ -0,0 +1,24 @@
+/*
+Create a faulty calculator using java script 
+This faulty calculator does following things:
+1. it takes two number from the user
+2. it performs wrong operations as follows
+
+ + ---> -
+ * ---> +
+ - ---> /
+ / ---> **
+
+3. it performs wrong operations 10% of times .
+*/
+
+if(Math.random() < 0.1){
+    switch () {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
