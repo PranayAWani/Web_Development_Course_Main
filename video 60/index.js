22dddf9c56061104a55286e093fd76549f52635d
PranayAWani <pranaywani04@gmail.com>
2023-12-28 19:55:30 +0530

    strings

---
+++ video 60/index.js
@@ -0,0 +1,32 @@
+// string tutorial
+console.log("This is strings tutorial");
+let a="Pranay"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+
+let real_name ="Pranay"
+let friend ="Nishant"
+
+console.log("hey hi my name is "+real_name +" and my friends name is "+ friend);
+console.log(`hey hi my name is &{real_name} and my friends name is &{friend}`); 
+
+console.log(a.toLowerCase());
+console.log(a.toUpperCase());
+
+//name slicing
+
+console.log(a.slice(2,4)); // only the words between 2 and 4 will be printed with 4th alphabet included and 2nd alphabet excluded
+
+// replacing
+
+console.log(a.replace ("Pranay","Vibhor"));
+
+// concard
+
+console.log(a.concat(friend));
