8dac9230e3dd1bf132a26f4aca71b15fdfcf176f
PranayAWani <pranaywani04@gmail.com>
2024-05-06 16:37:38 +0530

    practice

--- video 60/index.js
+++ video 60/index.js
@@ -1,32 +1,18 @@
-// string tutorial
-console.log("This is strings tutorial");
-let a="Pranay"
+// Strings tutorial 
+console.log("This is a strings tutorial")
+let a = "Pranay";
 console.log(a[0]);
 console.log(a[1]);
-console.log(a[2]);
 console.log(a[3]);
+console.log(a[2]);
 console.log(a[4]);
-console.log(a[5]);
 
-console.log(a.length);
-
-let real_name =" Pranay"
-let friend =" Nishant"
-
-console.log("hey hi my name is "+real_name +" and my friends name is "+ friend);
-console.log(`hey hi my name is &{real_name} and my friends name is &{friend}`); 
-
-console.log(a.toLowerCase());
-console.log(a.toUpperCase());
-
-//name slicing
-
-console.log(a.slice(2,4)); // only the words between 2 and 4 will be printed with 4th alphabet included and 2nd alphabet excluded
-
-// replacing
-
-console.log(a.replace ("Pranay","Vibhor"));
-
-// concard
-
-console.log(a.concat(friend," Shlok"," Kavish"));
+console.log(a.length)
+let first_name = "Pranay";
+let real_name ="Praddumna";
+console.log("his name is " + first_name + " but his real name is " + real_name)
+console.log(`His name is ${first_name} and his real name is ${real_name}`)
+let b="Nishant"
+console.log(b.toUpperCase())
+let c="ShLoK"
+console.log(c.toLowerCase())
