62e2a9d551bb55b9a7613daae77fec29a048fd70
PranayAWani <pranaywani04@gmail.com>
2024-12-16 20:26:41 +0530

    update

---
+++ video 55/script.js
@@ -0,0 +1,32 @@
+alert("hello");
+console.log("Hey this is tutorial 2 of js in sigma web development course");
+var a =5;
+var b=6;
+var c="Pranay";
+
+console.log(typeof a, typeof b, typeof c);
+
+console.log(a+b+8);
+
+const a1=6;
+
+let x= "Pranay";
+let y = 33;
+let z= 4.78;
+const p= true;
+let q= undefined;
+let r= null ;
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o= {
+    name: "Pranay";
+    "job roll": "Software Developer";
+    "age": 25;
+    "job code": 1232;
+}
+
+console.log(o);
+o.salary = "500cr";
+console.log(o);
+
